fa25e0b1dfa7bfbd958ab257fd458dcf21c4ac35
Pablo Bollansée <pablo.bollansee@gmail.com>
2014-03-06 18:44:32 +0100

    Separated the fluid-solver-logic from the used arrays.

---
+++ Unity/Assets/Resources/Fluid/FluidLayer.cs
@@ -0,0 +1,191 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluidLayer : MonoBehaviour {
+
+	const int N = FluidSolver.N;
+
+	public float _fluidity = 0.0001f;
+	public float _diffusion = 0.0001f;
+
+	float[][]
+		_horSpeed = new float[N+2][],
+		_verSpeed = new float[N+2][],
+		_density = new float[N+2][],
+
+		_horSpeedSource = new float[N+2][],
+		_verSpeedSource = new float[N+2][],
+		_densitySource = new float[N+2][],
+
+		_gradient = new float[N+2][];
+
+	float _totalDens;
+
+	Mesh _mesh;
+	Vector3 _prevMouseWorldPos;
+
+	//Performance test
+	Timer _fluidTimer = new Timer();
+	//
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < N+2; ++i) {
+			_horSpeed[i] = new float[N+2];
+			_verSpeed[i] = new float[N+2];
+			_density[i] = new float[N+2];
+
+			_horSpeedSource[i] = new float[N+2];
+			_verSpeedSource[i] = new float[N+2];
+			_densitySource[i] = new float[N+2];
+
+			_gradient[i] = new float[N+2];
+		}
+
+		// Initialize visuals
+        renderer.material.mainTexture = new Texture2D (N+2, N+2, TextureFormat.ARGB32, false);
+		_mesh = new Mesh();
+		GetComponent<MeshFilter> ().mesh = _mesh;
+		//(collider as MeshCollider).sharedMesh = _mesh;
+		CreatePlaneMesh (10);
+	}
+
+	void CreatePlaneMesh(float size) {
+		var m = _mesh;
+		m.name = "Plane";
+
+		int hCount2 = N+2;
+		int vCount2 = N+2;
+		int numTriangles = (N+1) * (N+1) * 6;
+		int numVertices = hCount2 * vCount2;
+		
+		Vector3[] vertices = new Vector3[numVertices];
+		Vector2[] uvs = new Vector2[numVertices];
+		int[] triangles = new int[numTriangles];
+		
+		int index = 0;
+		float uvFactorX = 1.0f/(N+1);
+		float uvFactorY = 1.0f/(N+1);
+		float scaleX = size/(N+1);
+		float scaleY = size/(N+1);
+		for (float y = 0.0f; y < vCount2; y++)
+		{
+			for (float x = 0.0f; x < hCount2; x++)
+			{
+				vertices[index] = new Vector3(x*scaleX - size/2, 0.0f, y*scaleY - size/2);
+				uvs[index++] = new Vector2(x*uvFactorX, y*uvFactorY);
+			}
+		}
+		
+		index = 0;
+		for (int y = 0; y < (N+1); y++)
+		{
+			for (int x = 0; x < (N+1); x++)
+			{
+				triangles[index]   = (y     * hCount2) + x;
+				triangles[index+1] = ((y+1) * hCount2) + x;
+				triangles[index+2] = (y     * hCount2) + x + 1;
+				
+				triangles[index+3] = ((y+1) * hCount2) + x;
+				triangles[index+4] = ((y+1) * hCount2) + x + 1;
+				triangles[index+5] = (y     * hCount2) + x + 1;
+				index += 6;
+			}
+		}
+		
+		m.vertices = vertices;
+		m.uv = uvs;
+		m.triangles = triangles;
+		m.RecalculateNormals();
+	}
+	void GenerateTexture() {
+		var texture = renderer.material.mainTexture as Texture2D;
+
+		// set the pixel values
+		for (int i = 0; i < N+2; ++i) {
+			for (int j = 0; j < N+2; ++j) {
+				float r = 0; //_horSpeed[i][j]/2;
+				float g = 0; //_verSpeed[i][j]/2;
+				float b = _density[i][j];
+				texture.SetPixel(i, j, new Color(r, g, b));
+			}
+		}
+		
+		// Apply all SetPixel calls
+		texture.Apply();
+	}
+	void ApplyDepthData() {
+		//var mesh = GetComponent<MeshFilter> ().sharedMesh;
+		var mesh = _mesh;
+		Vector3[] vertices = mesh.vertices;
+		//int[] triangles = _mesh.triangles;
+
+		for (int i = 0; i < N+2; ++i) {
+			for (int j = 0; j < N+2; ++j) {
+				int index = FluidSolver.CalculateIndex(i,j);
+				vertices[index].y = _density[i][j];
+			}
+		}
+
+		mesh.vertices = vertices;
+		//_mesh.triangles = triangles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FluidUpdate();
+
+		GenerateTexture ();
+		ApplyDepthData ();
+	}
+
+	void FluidUpdate() {
+		float dt = Time.deltaTime;
+
+		_totalDens = 0;
+		for (int i=0 ; i<N+2 ; i++ ) { 
+			for (int j=0 ; j<N+2 ; j++ ) {
+				_densitySource[i][j] = 0;
+				_horSpeedSource[i][j] = 0; //1.0f;
+				_verSpeedSource[i][j] = 0; //1.0f;
+				_totalDens += _density[i][j];
+			}
+		}
+
+
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (collider.Raycast(ray, out hit, float.PositiveInfinity)) {
+				_prevMouseWorldPos = hit.point;
+			}
+		}
+		if (Input.GetMouseButton (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (collider.Raycast(ray, out hit, float.PositiveInfinity)) {
+				Vector2 hitpos = (Vector2.one - hit.textureCoord) * N;
+				int x = Mathf.RoundToInt(hitpos.x);
+				int y = Mathf.RoundToInt(hitpos.y);
+				if (x > 10 && x < N-10 && y > 10 && y < N-10) {
+					_densitySource[x][y] += 100;
+
+					Vector3 move = (hit.point - _prevMouseWorldPos) * 10000;
+					_horSpeedSource[x][y] += move.x;
+					_verSpeedSource[x][y] += move.z;
+
+					_prevMouseWorldPos = hit.point;
+				}
+			}
+		}
+
+		_fluidTimer.Start();
+		FluidSolver.UpdateFluid(N, _horSpeed, _verSpeed, _density, _horSpeedSource, _verSpeedSource, _densitySource, _fluidity, _diffusion, dt);
+		_fluidTimer.Stop();
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(10,10, 500, 30), "Fluid: " + _fluidTimer);
+		GUI.Label(new Rect(10,30, 500, 30), "Total Density: " + _totalDens);
+	}
+}
--- Unity/Assets/Resources/Fluid/FluidSolver.cs
+++ Unity/Assets/Resources/Fluid/FluidSolver.cs
@@ -4,239 +4,50 @@ using System.Linq;
 
 // Paper used: http://www.dgp.toronto.edu/people/stam/reality/Research/pdf/GDC03.pdf
 
-public class FluidSolver : MonoBehaviour {
+public static class FluidSolver {
+	public const int N = 100;
+	public const int size=(N+2)*(N+2);
 
-	// Performance check
-	Timer _fluidTimer = new Timer ();
-	Timer _visualsTimer = new Timer ();
-	Timer _extraTimer = new Timer();
-	Timer _diffuseTimer = new Timer();
-	// 
-
-	const int N = 100;
-	const int size=(N+2)*(N+2);
-
-	float[][]
-	u = new float[N+2][],
-	v = new float[N+2][],
-	u_prev = new float[N+2][],
-	v_prev = new float[N+2][],
-		
-	dens = new float[N+2][],
-	dens_prev = new float[N+2][];
-
-	float[][]
-	u_source = new float[N+2][],
-	v_source = new float[N+2][],
-	dens_source = new float[N+2][];
-    
-	Mesh _mesh;
-	Vector3 _prevMouseWorldPos;
+	static float[][]
+		temp = new float[N+2][],
+		temp2 = new float[N+2][];
 
 	// Use this for initialization
-	void Start () {
+	static FluidSolver() {
 		// Initialize all jagged arrays
 		for (int i = 0; i < N+2; ++i) {
-			u[i] = new float[N+2];
-			v[i] = new float[N+2];
-			u_prev[i] = new float[N+2];
-			v_prev[i] = new float[N+2];
-
-			dens[i] = new float[N+2];
-			dens_prev[i] = new float[N+2];
-
-			u_source[i] = new float[N+2];
-			v_source[i] = new float[N+2];
-			dens_source[i] = new float[N+2];
+			temp[i] = new float[N+2];
+			temp2[i] = new float[N+2];
 		}
+	}
 
-		// Add some test-density
-		for (int i=1 ; i<=N ; i++ ) { 
-			for (int j=1 ; j<=N ; j++ ) {
-				dens[i][j] = 0;//Mathf.Max( ((float)(j)*2/(float)N)-1, 0);
-			}
-		}
-        
-		// Initialize visuals
-        renderer.material.mainTexture = new Texture2D (N+2, N+2, TextureFormat.ARGB32, false);
-		_mesh = new Mesh();
-		GetComponent<MeshFilter> ().mesh = _mesh;
-		//(collider as MeshCollider).sharedMesh = _mesh;
-		CreatePlaneMesh (10);
+	static public void UpdateFluid(int N, float[][] horSpeed, float[][] verSpeed, float[][] dens, float[][] horSpeedSource, float[][] verSpeedSource, float[][] densSource, float flui, float diff, float dt) {
+		VelocityStep (N, horSpeed, verSpeed, horSpeedSource, verSpeedSource, flui, dt);
+		DensityStep (N, dens, densSource, horSpeed, verSpeed, diff, dt);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		_fluidTimer.Start ();
-		FluidUpdate ();
-		_fluidTimer.Stop ();
-
-		_visualsTimer.Start ();
-		GenerateTexture ();
-		ApplyDepthData ();
-		_visualsTimer.Stop ();
-	}
-
-	void OnGUI () {
-		GUI.Label (new Rect (10, 10, 600, 20), "Fluid " + _fluidTimer);
-		GUI.Label (new Rect (10, 30, 600, 20), "Visua " + _visualsTimer);
-		GUI.Label (new Rect (10, 70, 600, 20), "Extra " + _extraTimer);
-		GUI.Label (new Rect (10, 90, 600, 20), "Diffu " + _diffuseTimer);
-	}
-
-	void CreatePlaneMesh(float size) {
-		var m = _mesh;
-		m.name = "Plane";
-
-		int hCount2 = N+2;
-		int vCount2 = N+2;
-		int numTriangles = (N+1) * (N+1) * 6;
-		int numVertices = hCount2 * vCount2;
-		
-		Vector3[] vertices = new Vector3[numVertices];
-		Vector2[] uvs = new Vector2[numVertices];
-		int[] triangles = new int[numTriangles];
-		
-		int index = 0;
-		float uvFactorX = 1.0f/(N+1);
-		float uvFactorY = 1.0f/(N+1);
-		float scaleX = size/(N+1);
-		float scaleY = size/(N+1);
-		for (float y = 0.0f; y < vCount2; y++)
-		{
-			for (float x = 0.0f; x < hCount2; x++)
-			{
-				vertices[index] = new Vector3(x*scaleX - size/2, 0.0f, y*scaleY - size/2);
-				uvs[index++] = new Vector2(x*uvFactorX, y*uvFactorY);
-			}
-		}
-		
-		index = 0;
-		for (int y = 0; y < (N+1); y++)
-		{
-			for (int x = 0; x < (N+1); x++)
-			{
-				triangles[index]   = (y     * hCount2) + x;
-				triangles[index+1] = ((y+1) * hCount2) + x;
-				triangles[index+2] = (y     * hCount2) + x + 1;
-				
-				triangles[index+3] = ((y+1) * hCount2) + x;
-				triangles[index+4] = ((y+1) * hCount2) + x + 1;
-				triangles[index+5] = (y     * hCount2) + x + 1;
-				index += 6;
-			}
-		}
-		
-		m.vertices = vertices;
-		m.uv = uvs;
-		m.triangles = triangles;
-		m.RecalculateNormals();
-	}
-
-	void ApplyDepthData() {
-		//var mesh = GetComponent<MeshFilter> ().sharedMesh;
-		var mesh = _mesh;
-		Vector3[] vertices = mesh.vertices;
-		//int[] triangles = _mesh.triangles;
-
-		for (int i = 0; i < N+2; ++i) {
-			for (int j = 0; j < N+2; ++j) {
-				int index = CalculateIndex(i,j);
-				vertices[index].y = dens[i][j];
-			}
-		}
-
-		mesh.vertices = vertices;
-		//_mesh.triangles = triangles;
-	}
-
-	void GenerateTexture() {
-		var texture = renderer.material.mainTexture as Texture2D;
-
-		// set the pixel values
-		for (int i = 0; i < N+2; ++i) {
-			for (int j = 0; j < N+2; ++j) {
-				float r = 0; //u[i][j]/2;
-				float g = 0; //v[i][j]/2;
-				float b = dens[i][j];
-				texture.SetPixel(i, j, new Color(r, g, b));
-			}
-		}
-		
-		// Apply all SetPixel calls
-		texture.Apply();
-	}
-
-	void FluidUpdate() {
-		float dt = Time.deltaTime;
-
-		for (int i=0 ; i<N+2 ; i++ ) { 
-			for (int j=0 ; j<N+2 ; j++ ) {
-				dens_prev[i][j] = 0;
-			}
-		}
-
-		for (int i=0 ; i<N+2 ; i++ ) { 
-			for (int j=0 ; j<N+2 ; j++ ) {
-				u_prev[i][j] = 0; //1.0f;
-				v_prev[i][j] = 0; //1.0f;
-			}
-		}
-
-
-		if (Input.GetMouseButtonDown (0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-			if (collider.Raycast(ray, out hit, float.PositiveInfinity)) {
-				_prevMouseWorldPos = hit.point;
-			}
-		}
-		if (Input.GetMouseButton (0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-			if (collider.Raycast(ray, out hit, float.PositiveInfinity)) {
-				Vector2 hitpos = (Vector2.one - hit.textureCoord) * N;
-				int x = Mathf.RoundToInt(hitpos.x);
-				int y = Mathf.RoundToInt(hitpos.y);
-				dens_prev[x][y] += 100;
-
-				Vector3 move = (hit.point - _prevMouseWorldPos) * 10000;
-				u_prev[x][y] += move.x;
-				v_prev[x][y] += move.z;
-
-				_prevMouseWorldPos = hit.point;
-			}
-		}
-
-		float visc = 0.0001f;
-		VelocityStep (N, u, v, u_prev, v_prev, visc, dt);
-
-		float diff = 0.00001f; //Diffusion, how well the particles spread out
-		DensityStep (N, dens, dens_prev, u, v, diff, dt);
-	}
-	
-	void DensityStep (int N, float[][] x, float[][] x0, float[][] u, float[][] v, float diff, float dt ) {
+	static void DensityStep (int N, float[][] x, float[][] x0, float[][] u, float[][] v, float diff, float dt ) {
 		//_extraTimer.Start (); 8.6
 
 		//_extraTimer.Start (); 0.07
 		AddSource ( N, x, x0, dt );
 		//_extraTimer.Stop ();
 
-		Swap (ref x0, ref x );
+		Swap (ref /*x0*/ temp, ref x );
 		//_extraTimer.Start (); //7.8
-		Diffuse ( N, 0, x, x0, diff, dt );
+		Diffuse ( N, 0, x, /*x0*/temp, diff, dt );
 		//_extraTimer.Stop ();
 
-		Swap (ref x0, ref x );
+		Swap (ref /*x0*/temp, ref x );
 		//_extraTimer.Start (); //0.66
-		Advect ( N, 0, x, x0, u, v, dt ); 
+		Advect ( N, 0, x, /*x0*/temp, u, v, dt ); 
 		//_extraTimer.Stop ();
 
 		//_extraTimer.Stop ();
 	}
 
 	
-	void VelocityStep (int N, float[][] u, float[][] v, float[][] u0, float[][] v0, float visc, float dt ) { 
+	static void VelocityStep (int N, float[][] u, float[][] v, float[][] u0, float[][] v0, float flui, float dt ) { 
 		//_extraTimer.Start (); //33.2
 
 		//_extraTimer.Start (); 0.14
@@ -245,41 +56,86 @@ public class FluidSolver : MonoBehaviour {
 		//_extraTimer.Stop ();
 
 		//_extraTimer.Start (); //15.4
-		Swap (ref u0, ref u );
-		Diffuse ( N, 1, u, u0, visc, dt ); 
+		Swap (ref /*u0*/temp, ref u );
+		Diffuse ( N, 1, u, /*u0*/temp, flui, dt ); 
 
-		Swap (ref v0, ref v );
-		Diffuse ( N, 2, v, v0, visc, dt );
+		Swap (ref /*v0*/temp2, ref v );
+		Diffuse ( N, 2, v, /*v0*/temp2, flui, dt );
 		//_extraTimer.Stop ();
 
 		//_extraTimer.Start (); //7.9
-		project ( N, u, v, u0, v0 ); 
+		project ( N, u, v, /*u0*/temp, /*v0*/temp2 ); 
 		//_extraTimer.Stop ();
 
-		Swap (ref u0, ref u );
-		Swap (ref v0, ref v );
+		Swap (ref /*u0*/temp, ref u );
+		Swap (ref /*v0*/temp2, ref v );
 
 		//_extraTimer.Start (); //1.3
-		Advect ( N, 1, u, u0, u0, v0, dt );
-		Advect ( N, 2, v, v0, u0, v0, dt ); 
+		Advect ( N, 1, u, /*u0*/temp, /*u0*/temp, /*v0*/temp2, dt );
+		Advect ( N, 2, v, /*v0*/temp2, /*u0*/temp, /*v0*/temp2, dt ); 
 		//_extraTimer.Stop ();
 
 		//_extraTimer.Start (); //8
-		project ( N, u, v, u0, v0 ); 
+		project ( N, u, v, /*u0*/temp, /*v0*/temp2 ); 
 		//_extraTimer.Stop ();
 
 		//_extraTimer.Stop ();
 	} 
 
-	void Diffuse ( int N, int b, float[][] x, float[][] x0, float diff, float dt ) {
-		_diffuseTimer.Start (); //7.8 ; 7.2 ; 
+	/*static void DiffuseDensity ( int N, int b, float[][] x, float[][] x0, float diff, float dt ) {
+		//_diffuseTimer.Start (); //7.8 ; 7.2 ; 
+		int k, i, j;
+		float a=dt*diff*N*N;
 
+		//bounds
+		for (i=1 ; i<=N ; ++i ) { 
+			x[0][i]		= (	x0[0][i] +		a*(	x[0][i-1] +		x[0][i+1] +		x[1][i]	))		/(1+3*a);
+			x[N+1][i]	= (	x0[N+1][i] +	a*(	x[N+1][i-1] +	x[N+1][i+1] +	x[N][i] ))		/(1+3*a);
+			x[i][0]		= (	x0[i][0] +		a*( x[i+1][0] +		x[i-1][0] +		x[i][1] ))		/(1+3*a);
+			x[i][N+1]	= (	x0[i][N+1] +	a*(	x[i-1][N+1] +	x[i+1][N+1] +	x[i][N]	))		/(1+3*a);
+		}
+		x[0][0]		= (x0[0][0]		+ a*(	x[1][0] 	+	x[0][1]		))	/(1+2*a); 
+		x[0][N+1]	= (x0[0][N+1]	+ a*(	x[1][N+1]	+	x[0][N]		))	/(1+2*a); 
+		x[N+1][0]	= (x0[N+1][0]	+ a*(	x[N][0]		+	x[N+1][1]	))	/(1+2*a);
+		x[N+1][N+1]	= (x0[N+1][N+1]	+ a*(	x[N][N+1]	+	x[N+1][N]	))	/(1+2*a);
+		
+		//middle
+		for (k=0 ; k<20 ; ++k ) {
+			//_extraTimer.Start(); 0.3
+			//SetBound ( N, b, x );
+
+			for (i=1 ; i<=N ; ++i ) { 
+				for (j=1 ; j<=N ; ++j ) { 
+					x[i][j] =
+						(
+							x0[i][j] +
+							a*(
+							x[i-1][j] +
+							x[i+1][j] +
+							x[i][j-1] +
+							x[i][j+1]
+							)
+						)/(1+4*a); 
+				} 
+			}
+			//_extraTimer.Stop();
+
+			//SetBound ( N, b, x );
+		}
+
+		//_diffuseTimer.Stop ();
+	}*/
+
+	static void Diffuse ( int N, int b, float[][] x, float[][] x0, float diff, float dt ) {
 		float a=dt*diff*N*N;
 		
-		for (int k=0 ; k<20 ; ++k ) {
+		int k, i, j;
+		for (k=0 ; k<20 ; ++k ) {
 			//_extraTimer.Start(); 0.3
-			for (int i=1 ; i<=N ; ++i ) { 
-				for (int j=1 ; j<=N ; ++j ) { 
+			//SetBound ( N, b, x );
+
+			for (i=1 ; i<=N ; ++i ) { 
+				for (j=1 ; j<=N ; ++j ) { 
 					x[i][j] =
 						(
 							x0[i][j] +
@@ -297,17 +153,17 @@ public class FluidSolver : MonoBehaviour {
 			SetBound ( N, b, x );
 
 		}
-
-		_diffuseTimer.Stop ();
 	}
-	
-	void Advect ( int N, int b, float[][] d, float[][] d0, float[][] u, float[][] v, float dt ) { 
+
+	static void Advect ( int N, int b, float[][] d, float[][] d0, float[][] u, float[][] v, float dt ) { 
 		int i0, j0, i1, j1; 
 		float x, y, s0, t0, s1, t1, dt0;
 
 		dt0 = dt*N;
-		for (int i=1 ; i<=N ; i++ ) { 
-			for (int j=1 ; j<=N ; j++ ) {
+
+		int i, j;
+		for (i=1 ; i<=N ; i++ ) { 
+			for (j=1 ; j<=N ; j++ ) {
 				x = i-dt0*u[i][j];
 				y = j-dt0*v[i][j]; 
 
@@ -326,7 +182,7 @@ public class FluidSolver : MonoBehaviour {
 				t1 = y-j0;
 				t0 = 1-t1;
 
-				float dOri = d[i][j];
+				//float dOri = d[i][j];
 				d[i][j] = 
 					s0*(
 						t0*d0[i0][j0] +
@@ -348,37 +204,39 @@ public class FluidSolver : MonoBehaviour {
 
 	//
 	// Helpers
-	void AddSource ( int N, float[][] x, float[][] s, float dt ) { 
+	static void AddSource ( int N, float[][] x, float[][] s, float dt ) { 
 		//int size=(N+2)*(N+2);
-		for (int i=0 ; i<N+2 ; i++ ) { 
-			for (int j=0 ; j<N+2 ; j++ ) { 
+		int i, j;
+		for (i=0 ; i<N+2 ; ++i ) { 
+			for (j=0 ; j<N+2 ; ++j ) { 
 				x [i][j] += dt * s [i][j];
 			}
 		}
 	} 
 
-	void project ( int N, float[][] u, float[][] v, float[][] p, float[][] div ) {
-		float h; 
+	static void project ( int N, float[][] u, float[][] v, float[][] p, float[][] div ) {
+		int k, i, j;
+		float h;
 		h = 1.0f/N; 
-		for (int i=1 ; i<=N ; i++ ) { 
-			for (int j=1 ; j<=N ; j++ ) { 
+		for (i=1 ; i<=N ; ++i ) { 
+			for (j=1 ; j<=N ; ++j ) { 
 				div[i][j] = -0.5f*h*(u[i+1][j]-u[i-1][j]+v[i][j+1]-v[i][j-1]); 
 				p[i][j] = 0; 
 			} 
 		} 
 		SetBound ( N, 0, div ); SetBound ( N, 0, p ); 
 		
-		for (int k=0 ; k<20 ; k++ ) { 
-			for (int i=1 ; i<=N ; i++ ) { 
-				for (int j=1 ; j<=N ; j++ ) { 
+		for (k=0 ; k<20 ; ++k ) { 
+			for (i=1 ; i<=N ; ++i ) { 
+				for (j=1 ; j<=N ; ++j ) { 
 					p[i][j] = (div[i][j]+p[i-1][j]+p[i+1][j]+ p[i][j-1]+p[i][j+1])/4; 
 				} 
 			} 
 			SetBound ( N, 0, p ); 
 		} 
 		
-		for (int i=1 ; i<=N ; i++ ) { 
-			for (int j=1 ; j<=N ; j++ ) { 
+		for (i=1 ; i<=N ; i++ ) { 
+			for (j=1 ; j<=N ; j++ ) { 
 				u[i][j] -= 0.5f*(p[i+1][j]-p[i-1][j])/h; 
 				v[i][j] -= 0.5f*(p[i][j+1]-p[i][j-1])/h; 
 			} 
@@ -386,24 +244,24 @@ public class FluidSolver : MonoBehaviour {
 		SetBound ( N, 1, u ); SetBound ( N, 2, v ); 
 	}
 	
-	void SetBound ( int N, int b, float[][] x ) {
+	static void SetBound ( int N, int b, float[][] x ) {
 		for (int i=1 ; i<=N ; i++ ) { 
 			x[0][i]		= 0; //b==1 ? -x[1][i] : x[1][i];
 			x[N+1][i]	= 0; //b==1 ? -x[N][i] : x[N][i];
 			x[i][0]		= 0; //b==2 ? -x[i][1] : x[i][1];
 			x[i][N+1]	= 0; //b==2 ? -x[i][N] : x[i][N];
-		} 
-		x[0][0]		= 0; //0.5f*(x[1][0]+x[0][1]); 
-		x[0][N+1]	= 0; //0.5f*(x[1][N+1]+x[0][N]); 
-		x[N+1][0]	= 0; //0.5f*(x[N][0]+x[N+1][1]); 
-		x[N+1][N+1]	= 0; //0.5f*(x[N][N+1]+x[N+1][N]);
+		}
+		x[0][0]		= 0; //0.5f*(	x[1][0]		+	x[0][1]		); 
+		x[0][N+1]	= 0; //0.5f*(	x[1][N+1]	+	x[0][N]		); 
+		x[N+1][0]	= 0; //0.5f*(	x[N][0]		+	x[N+1][1]	); 
+		x[N+1][N+1]	= 0; //0.5f*(	x[N][N+1]	+	x[N+1][N]	);
 	} 
 
-	int CalculateIndex(int i, int j) {
+	public static int CalculateIndex(int i, int j) {
 		return ((i) + (N + 2) * (j));
 	}
 
-	void Swap(ref float[][] x0, ref float[][] x) {
+	static void Swap(ref float[][] x0, ref float[][] x) {
 		var tmp=x0;
 		x0=x;
 		x=tmp;
